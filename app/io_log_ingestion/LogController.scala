655590854d0a066cd21a996fa6b2c94e8a3865c7
Marcus Lucas <marcus_lucas12@outlook.com>
2019-01-23 23:51:18 -0300

    Add basic assertation for LogControllerTest

--- app/io_log_ingestion/LogController.scala
+++ app/io_log_ingestion/LogController.scala
@@ -3,11 +3,15 @@ package io_log_ingestion
 import com.google.inject.{Inject, Singleton}
 import play.api.http.HttpVerbs
 import play.api.mvc.AnyContent
-import play.mvc.{Action, Controller}
+import play.api.mvc._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 @Singleton
 class LogController @Inject() extends Controller with HttpVerbs  {
   def add: Action[AnyContent] = Action.async { implicit request =>
-    request.body
+    request.body match {
+      case _ => Future {Ok("asd")}
+    }
   }
 }
--- test/integration/io_log_ingestion/LogControllerTest.scala
+++ test/integration/io_log_ingestion/LogControllerTest.scala
@@ -2,19 +2,19 @@ package integration.io_log_ingestion
 
 import common.builders.IOLogPayloadBuilder
 import io_log_ingestion.LogController
-import org.scalatest.FlatSpec
+import org.scalatest.{FlatSpec, Matchers}
 import play.api.libs.json.Json
 import play.api.mvc.Result
 import play.api.test.FakeRequest
-import play.api.test.Helpers.{POST, contentAsString}
+import play.api.test.Helpers._
 
 import scala.concurrent.Future
 
-class LogControllerTest extends FlatSpec {
+class LogControllerTest extends FlatSpec with Matchers {
 
   val controller = new LogController()
 
-  behavior of "receiving a json payload"
+  behavior of "receiving a wise log"
 
   it should "successfully handle the request" in {
     val payload = IOLogPayloadBuilder().build
@@ -22,10 +22,10 @@ class LogControllerTest extends FlatSpec {
     val request = FakeRequest(POST, "/log").withJsonBody(Json.parse(payload))
 
     val result: Future[Result] = controller.add().apply(request)
+
     val bodyText: String = contentAsString(result)
-    val actualLogEvent = Json.parse(bodyText).as[LogEvent]
 
-
+    status(result) shouldEqual OK
   }
 
 }
